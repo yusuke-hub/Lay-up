22e4322355e823b265224b3d4647dd5fc07ce1ee
yusuke-hub <yk1210140408@gmail.com>
2020-01-11 10:24:27 +0000

    fixed session form & about to change column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  def after_sign_in_path_for(resource)
+  	users_index_path
+  end
+  def afrter_sign_out_path_for(resource)
+  end
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :account_id])
