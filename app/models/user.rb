b2c95ec89cc56decdf70c23c1137b6e2c07270ae
yusuke-hub <yk1210140408@gmail.com>
2020-01-11 08:50:32 +0000

    validation & configure_permitted_parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :account_id, uniqueness: true, length: { in: 8..12 }
 end
