c55a6143825b0afa711d0acdae2b9ffc99be2e16
yusuke-hub <yk1210140408@gmail.com>
2020-01-11 09:59:19 +0000

    breed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  [
+    {
+      account_id: '11111111',
+      email: 'test1@test.com',
+      password: '111111',
+      name: '田中太郎',
+    },
+    {
+      account_id: '22222222',
+      email: 'test2@test.com',
+      password: '222222',
+      name: '田中徹子',
+    },
+    {
+      account_id: '333333333',
+      email: 'test3@test.com',
+      password: '333333',
+      name: '佐藤太郎',
+    },
+    {
+      account_id: '44444444',
+      email: 'test4@test.com',
+      password: '444444',
+      name: '佐藤徹子',
+    },
+    {
+      account_id: '55555555',
+      email: 'test5@test.com',
+      password: '555555',
+      name: '小林太郎',
+    },
+    {
+      account_id: '66666666',
+      email: 'test6@test.com',
+      password: '666666',
+      name: '小林徹子',
+    },
+    {
+      account_id: '77777777',
+      email: 'test7@test.com',
+      password: '777777',
+      name: '伊藤太郎',
+    },
+    {
+      account_id: '88888888',
+      email: 'test8@test.com',
+      password: '888888',
+      name: '伊藤徹子',
+    }
+  ]
+)
+
+Group.create!(
+	[
+		{
+	  name: '田中家',
+	  caption: '田中家のグループ',
+		},
+		{
+	  name: '佐藤家',
+	  caption: '佐藤家のグループ',
+		},
+		{
+	  name: '小林家',
+	  caption: '小林家のグループ',
+		},
+		{
+	  name: '伊藤家',
+	  caption: '伊藤家のグループ',
+		}
+	]
+)
