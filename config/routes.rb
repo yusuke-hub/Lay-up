6edfdb39a0df986074146ab51dbb6bf5e6a7b1b6
yusuke-hub <yk1210140408@gmail.com>
2020-01-12 01:48:11 +0000

    added header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'plans/index'
+  get 'plans/show'
+  get 'plans/edit'
+  get 'homes/about'
   get 'users/index'
   get 'users/show'
   get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index,:show,:edit,:update]
+  resources :plans, only: [:index,:create,:show,:edit,:update,:destroy]
 end
---
+++ test/controllers/plans_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PlansControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get plans_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get plans_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get plans_edit_url
+    assert_response :success
+  end
+
+end
