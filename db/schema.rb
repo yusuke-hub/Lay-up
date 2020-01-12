d67f5a31beb6a8db197f86cbd08b7ceddbe1d48e
yusuke-hub <yk1210140408@gmail.com>
2020-01-12 22:37:43 +0000

    remove group_id column from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_223249) do
+ActiveRecord::Schema.define(version: 2020_01_12_223653) do
 
   create_table "add_user_to_groups", force: :cascade do |t|
     t.integer "user_id"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2020_01_12_223249) do
     t.datetime "remember_created_at"
     t.string "name"
     t.string "account_id"
-    t.integer "group_id"
     t.text "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
