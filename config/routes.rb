99ef8b0c52a14a8e04f385a586c2b94898baa92f
yusuke-hub <yk1210140408@gmail.com>
2020-01-12 03:36:36 +0000

    added groups routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  get 'groups/index'
-  get 'groups/edit'
-  get 'groups/show'
-  get 'plans/index'
-  get 'plans/show'
-  get 'plans/edit'
-  get 'homes/about'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'homes/about'
   resources :users, only: [:index,:show,:edit,:update]
   resources :plans, only: [:index,:create,:show,:edit,:update,:destroy]
+  resources :groups, only: [:index,:create,:show,:edit,:update,:destroy]
 end
