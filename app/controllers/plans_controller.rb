9349464ccdaaf1d1eed3492dd0c8df7f713d2f35
yusuke-hub <yk1210140408@gmail.com>
2020-01-20 01:03:35 +0000

    added js&html&controller for plan#new

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,6 +3,11 @@ class PlansController < ApplicationController
     @plans = Plan.where(user_id: current_user.id)
   end
 
+  def create
+    @plan = Plan.new(plan_params)
+    @plan.save!
+  end 
+
   def show
     @plan = Plan.find(params[:id])
   end
@@ -11,6 +16,6 @@ class PlansController < ApplicationController
   end
   private
   def plan_params
-    params.require(:plan).permit(:user_id,:situation_id,:d_latitude,:d_name,:s_latitude,:s_longitude,:s_name,:distance,:altitude)
+    params.require(:plan).permit(:user_id, :situation_id, :departure, :shelter, :distance, :duration, :memo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  :to => 'homes#about'
   resources :users, only: [:index,:show,:edit,:update]
-  resources :plans, only: [:index,:create,:show,:edit,:update,:destroy]
+  resources :plans, only: [:index,:new, :create,:show,:edit,:update,:destroy]
   resources :groups, only: [:new,:index,:create,:show,:edit,:update,:destroy]
 end
