01ed57148ea004259493034909d84a7853dca332
yusuke-hub <yk1210140408@gmail.com>
2020-01-22 00:45:31 +0000

    fixed spell afrter_sign_out_path / application.controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
   	root_path
   end
-  def afrter_sign_out_path_for(resource)
+  def after_sign_out_path_for(resource)
     new_user_session_path
   end
   protected
