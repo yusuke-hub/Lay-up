0b119d2dd6225bac7792eda8dd3e42363fae1d19
yusuke-hub <yk1210140408@gmail.com>
2020-01-25 10:05:31 +0000

    fixed before_action authenticate user! home#about

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:create]
+  before_action :authenticate_user!, except: [:create, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :check_invitation
   def after_sign_in_path_for(resource)
