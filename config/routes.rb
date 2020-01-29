3a0e46a5772de48bbfcb31127ae24d5788803345
yusuke-hub <yk1210140408@gmail.com>
2020-01-29 05:58:05 +0000

    deleted home#about

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'homes#top'
-  get '/home/about', to: 'homes#about', as: 'home_about'
   get '/plans/detail/:id', to: 'plans#detail', as: 'plan_detail'
   get '/groups/confirm', to: 'groups#confirm', as: 'group_confirm'
   resources :users, only: [:index,:show,:edit, :destroy, :update]
